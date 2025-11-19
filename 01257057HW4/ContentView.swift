eacaac18cec628d9f11d582a4a3b51b1bf955970
ken204 <Kenny20041223@gmail.com>
2025-11-19 11:30:10 +0800

    Initial Commit

---
+++ 01257057HW4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01257057HW4
+//
+//  Created by user05 on 2025/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01257057HW4/_1257057HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  _1257057HW4App.swift
+//  01257057HW4
+//
+//  Created by user05 on 2025/11/19.
+//
+
+import SwiftUI
+
+@main
+struct _1257057HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
